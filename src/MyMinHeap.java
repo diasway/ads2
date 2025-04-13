593c8dc9b1f0caff31be9d28ea60e743bd43a4e6
diasway <mdias2006s@gmail.com>
2025-04-13 21:43:34 +0500

    5 commit MyMinHeap

--- src/MyMinHeap.java
+++ src/MyMinHeap.java
@@ -1,4 +1,76 @@
-public class MyMinHeap<T> {
-    private MyArrayList<T> list = new MyArrayList<>();
+import interfaces.MyList;
 
+public class MyMinHeap<T extends Comparable<T>> {
+    private final MyArrayList<T> heap;
+
+    public MyMinHeap() {
+        heap = new MyArrayList<>();
+    }
+
+    public void add(T item) {
+        heap.add(item);
+        heapifyUp(heap.size() - 1);
+    }
+
+    public T peek() {
+        if (isEmpty()) throw new IllegalStateException("Heap is empty");
+        return heap.get(0);
+    }
+
+    public T poll() {
+        if (isEmpty()) throw new IllegalStateException("Heap is empty");
+        T result = heap.get(0);
+        T last = heap.get(heap.size() - 1);
+        heap.set(0, last);
+        heap.removeLast();
+        heapifyDown(0);
+        return result;
+    }
+
+    public boolean isEmpty() {
+        return heap.size() == 0;
+    }
+
+    public int size() {
+        return heap.size();
+    }
+
+    private void heapifyUp(int index) {
+        while (index > 0) {
+            int parent = (index - 1) / 2;
+            if (heap.get(index).compareTo(heap.get(parent)) < 0) {
+                swap(index, parent);
+                index = parent;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void heapifyDown(int index) {
+        int left, right, smallest;
+        while ((left = 2 * index + 1) < heap.size()) {
+            right = left + 1;
+            smallest = index;
+
+            if (heap.get(left).compareTo(heap.get(smallest)) < 0) {
+                smallest = left;
+            }
+            if (right < heap.size() && heap.get(right).compareTo(heap.get(smallest)) < 0) {
+                smallest = right;
+            }
+            if (smallest != index) {
+                swap(index, smallest);
+                index = smallest;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void swap(int i, int j) {
+        T temp = heap.get(i);
+        heap.set(i, heap.get(j));
+        heap.set(j, temp);
+    }
 }
