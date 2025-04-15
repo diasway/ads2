398a02a94644104f00b8ff2cf1b6e851d58ff190
diasway <mdias2006s@gmail.com>
2025-04-15 15:28:58 +0500

    proverka

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-
 public class Main {
     public static void main(String[] args) {
+        MyArrayList<Integer> list1 = new MyArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        System.out.println(list1.get(2));
 
+        MyQueue<String> queue = new MyQueue<>();
+        queue.enqueue("SE-2408");
+        System.out.println(queue.dequeue());
+
+        MyStack<String> stack = new MyStack<>();
+        stack.push("Dias");
+        stack.push("Mukhametkali");
+        System.out.println(stack.pop());
+
+        MyMinHeap<Integer> minHeap = new MyMinHeap<>();
+        minHeap.add(8);
+        minHeap.add(1);
+        minHeap.add(2);
     }
 }
